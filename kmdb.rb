c449de39bbdae3878e7166993b19fa466f9f62bb
weillium <will.liao@kellogg.northwestern.edu>
2022-02-03 19:28:12 +0000

    associations

--- kmdb.rb
+++ kmdb.rb
@@ -89,89 +89,152 @@ Role.destroy_all
 #   t.integer :movie_id
 #   t.integer :actor_id
 #   t.string :character_name
+#
+# then add associations:
+# movie has_many: roles
+# role belongs_to: movie
 
 # Insert data into your database that reflects the sample data shown above
 # Do not use hard-coded foreign key IDs.
-# insert data into movies:
+
+# Batman Begins
 values = {title: "Batman Begins", year_released: 2005, rated: "PG-13"}
 movie = Movie.new(values)
 movie.save
 
+role = Role.new({character_name: "Bruce Wayne", movie_id: movie.id })
+role.save
+role = Role.new({character_name: "Alfred", movie_id: movie.id })
+role.save
+role = Role.new({character_name: "Ra's Al Ghul", movie_id: movie.id })
+role.save
+role = Role.new({character_name: "Rachel Dawes", movie_id: movie.id })
+role.save
+role = Role.new({character_name: "Commissioner Gordon", movie_id: movie.id })
+role.save
+
+# The Dark Knight
 values = {title: "The Dark Knight", year_released: 2008, rated: "PG-13"}
 movie = Movie.new(values)
 movie.save
 
+role = Role.new({character_name: "Bruce Wayne", movie_id: movie.id })
+role.save
+role = Role.new({character_name: "Joker", movie_id: movie.id })
+role.save
+role = Role.new({character_name: "Harvey Dent", movie_id: movie.id })
+role.save
+role = Role.new({character_name: "Alfred", movie_id: movie.id })
+role.save
+role = Role.new({character_name: "Rachel Dawes", movie_id: movie.id })
+role.save
+
+# The Dark Knight Rises
 values = {title: "The Dark Knight Rises", year_released: 2012, rated: "PG-13"}
 movie = Movie.new(values)
 movie.save
 
-# insert data into people:
+role = Role.new({character_name: "Bruce Wayne", movie_id: movie.id })
+role.save
+role = Role.new({character_name: "Commissioner Gordon", movie_id: movie.id })
+role.save
+role = Role.new({character_name: "Bane", movie_id: movie.id })
+role.save
+role = Role.new({character_name: "John Blake", movie_id: movie.id })
+role.save
+role = Role.new({character_name: "Selina Kyle", movie_id: movie.id })
+role.save
+
+# link director with movie:
 person = Person.new({name: "Christopher Nolan"})
 person.save
 
+movie = Movie.where({title: "Batman Begins"})[0]
+movie.director_id = person.id
+movie.save
+movie = Movie.where({title: "The Dark Knight"})[0]
+movie.director_id = person.id
+movie.save
+movie = Movie.where({title: "The Dark Knight Rises"})[0]
+movie.director_id = person.id
+movie.save
+
+# link person with role
+# Batman Begins
+movie = Movie.where({title: "Batman Begins"})[0]
+
 person = Person.new({name: "Christian Bale"})
 person.save
+role = Role.where({character_name: "Bruce Wayne"})[0]
+role.actor_id = person.id
+role.save
 
 person = Person.new({name: "Michael Caine"})
 person.save
+role = Role.where({character_name: "Alfred"})[0]
+role.actor_id = person.id
+role.save
 
 person = Person.new({name: "Liam Neeson"})
 person.save
+role = Role.where({character_name: "Ra's Al Ghul"})[0]
+role.actor_id = person.id
+role.save
 
 person = Person.new({name: "Katie Holmes"})
 person.save
+role = Role.where({character_name: "Rachel Dawes"})[0]
+role.actor_id = person.id
+role.save
 
 person = Person.new({name: "Gary Oldman"})
 person.save
+role = Role.where({character_name: "Commissioner Gordon"})[0]
+role.actor_id = person.id
+role.save
+
+# The Dark Knight
+movie = Movie.where({title: "The Dark Knight"})[0]
 
 person = Person.new({name: "Heath Ledger"})
 person.save
+role = Role.where({character_name: "Joker"})[0]
+role.actor_id = person.id
+role.save
 
 person = Person.new({name: "Aaron Eckhart"})
 person.save
+role = Role.where({character_name: "Harvey Dent"})[0]
+role.actor_id = person.id
+role.save
 
 person = Person.new({name: "Maggie Gyllenhaal"})
 person.save
+role = Role.where({character_name: "Rachel Dawes", movie_id: movie.id})[0]
+role.actor_id = person.id
+role.save
+
+# The Dark Knight Rises
+movie = Movie.where({title: "The Dark Knight Rises"})[0]
 
 person = Person.new({name: "Tom Hardy"})
 person.save
+role = Role.where({character_name: "Bane"})[0]
+role.actor_id = person.id
+role.save
 
 person = Person.new({name: "Joseph Gordon-Levitt"})
 person.save
+role = Role.where({character_name: "John Blake"})[0]
+role.actor_id = person.id
+role.save
 
 person = Person.new({name: "Anne Hathaway"})
 person.save
-
-# insert data into roles:
-role = Role.new({character_name: "Bruce Wayne"})
+role = Role.where({character_name: "Selina Kyle"})[0]
+role.actor_id = person.id
 role.save
 
-role = Role.new({character_name: "Alfred"})
-role.save
-
-role = Role.new({character_name: "Ra's Al Ghul"})
-role.save
-
-role = Role.new({character_name: "Rachel Dawes"})
-role.save
-
-role = Role.new({character_name: "Commissioner Gordon"})
-role.save
-
-role = Role.new({character_name: "Joker"})
-role.save
-
-role = Role.new({character_name: "Harvey Dent"})
-role.save
-
-role = Role.new({character_name: "Bane"})
-role.save
-
-role = Role.new({character_name: "John Blake"})
-role.save
-
-role = Role.new({character_name: "Selina Kyle"})
-role.save
 
 # Prints a header for the movies output
 puts "Movies"
@@ -179,7 +242,9 @@ puts "======"
 puts ""
 
 # Query the movies data and loop through the results to display the movies output
-# TODO!
+for movie in Movie
+    puts "#{movie.title}        #{movie.year}       #{movie.rated}     #{movie.director_id}"
+end
 
 # Prints a header for the cast output
 puts ""
