30034c4f3f42d171903ef60ff346e19df243877d
weillium <will.liao@kellogg.northwestern.edu>
2022-02-03 18:03:33 +0000

    save 2

--- kmdb.rb
+++ kmdb.rb
@@ -92,7 +92,58 @@ Role.destroy_all
 
 # Insert data into your database that reflects the sample data shown above
 # Do not use hard-coded foreign key IDs.
-# TODO!
+# insert data into movies:
+values = {title: "Batman Begins", year_released: 2005, rated: "PG-13"}
+movie = Movie.new(values)
+movie.save
+
+values = {title: "The Dark Knight", year_released: 2008, rated: "PG-13"}
+movie = Movie.new(values)
+movie.save
+
+values = {title: "The Dark Knight Rises", year_released: 2012, rated: "PG-13"}
+movie = Movie.new(values)
+movie.save
+
+# insert data into people:
+person = Person.new({name: "Christopher Nolan"})
+person.save
+
+person = Person.new({name: "Christian Bale"})
+person.save
+
+person = Person.new({name: "Michael Caine"})
+person.save
+
+person = Person.new({name: "Liam Neeson"})
+person.save
+
+person = Person.new({name: "Katie Holmes"})
+person.save
+
+person = Person.new({name: "Gary Oldman"})
+person.save
+
+person = Person.new({name: "Heath Ledger"})
+person.save
+
+person = Person.new({name: "Aaron Eckhart"})
+person.save
+
+person = Person.new({name: "Maggie Gyllenhaal"})
+person.save
+
+person = Person.new({name: "Tom Hardy"})
+person.save
+
+person = Person.new({name: "Joseph Gordon-Levitt"})
+person.save
+
+person = Person.new({name: "Anne Hathaway"})
+person.save
+
+# insert data into roles:
+
 
 # Prints a header for the movies output
 puts "Movies"
